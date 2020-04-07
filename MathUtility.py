fe26f36edbd237fa69b5e887457a396950a80653
nikhilrajpandey <nikhilrajpandey1@gmail.com>
2020-04-07 16:11:36 +0530

    Initialised Repo

---
+++ MathUtility.py
@@ -0,0 +1,43 @@
+class Fraction:
+    def __init__(self,numenator,denominator):
+        pass
+    def multiputive_inverse(self):
+        pass
+    def additive_inverse(self):
+        pass
+    def is_mixed_fraction(self):
+        pass
+    def convert_into_mix_fraction(self):
+        pass
+
+class Calculation:
+    # In any Function of this class numbers arg will always be a list
+    @classmethod
+    def divide(cls,divisor,dividend):
+        pass
+
+    @classmethod
+    def hcf(cls,numbers):
+        pass
+
+    @classmethod
+    def lcm(cls,numbers):
+        pass
+
+    @classmethod
+    def isPrime(cls,numbers):
+        pass
+
+    @classmethod
+    def is_perfect_square(cls,number):
+        pass
+
+    @classmethod
+    def square_root(cls,number):
+        pass
+
+    @classmethod
+    def cube_root(cls,number):
+        pass
+
+
